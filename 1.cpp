a21e40aad11437cb565ac14f21e17b27b5515733
Felipe Eiji Maruyama <felipe-eiji@hotmail.com>
2022-10-11 22:42:05 -0300

    solution in c++ for UVa 12662

--- 1.cpp
+++ 1.cpp
@@ -2,24 +2,69 @@
 using namespace std;
 
 int main(){
+    char students[101][4];
     int N;
-    vector<int> arrows(1000001,0);
-    int count = 0;
+
     scanf("%d", &N);
-    while(N--){
-        int input;
-        scanf("%d", &input);
-        if(arrows[input] == 0){
-            arrows[input-1]++;
+    for(int i = 0; i<N; i++){
+        scanf(" %s", students[i]);
+    }
+
+    int Q;
+
+    scanf("%d", &Q);
+
+    char left[4];
+    int leftDist;
+    char right[4];
+    int rightDist;
+
+    int pos;
+
+    while(Q--){
+        scanf("%d", &pos);
+        int tempPos = pos-1;
+
+        if(students[tempPos][0] != '?'){
+            printf("%s\n", students[tempPos]);
+            continue;
+        }
+        leftDist = tempPos - 1;
+        rightDist = tempPos + 1;
+
+        bool leftFound = false;
+        bool rightFound = false;
+
+        while(leftDist >= 0){
+            if(students[leftDist][0] != '?') {
+                strcpy(left,students[leftDist]);
+                leftFound = true;
+                break;
+            }
+            leftDist--;
+        }
+
+        while(rightDist < N){
+            if(students[rightDist][0] != '?'){
+                strcpy(right,students[rightDist]);
+                rightFound = true;
+                break;
+            } 
+            rightDist++;
+        }
+
+        if(leftFound && rightFound && ((tempPos - leftDist) == (rightDist - tempPos))){
+            printf("middle of %s and %s\n", left, right);
+        }else if((leftFound && ((tempPos - leftDist) < (rightDist - tempPos))) || !rightFound){
+            for(int i = 0; i<(tempPos-leftDist);i++){
+                printf("right of ");
+            }
+            printf("%s\n",left);
         }else{
-            arrows[input-1]++;
-            arrows[input]--;
+            for(int i = 0; i<(rightDist - tempPos);i++){
+                printf("left of ");
+            }
+            printf("%s\n",right);
         }
     }
-
-    for(int i = 0; i<1000000;i++){
-        count += arrows[i];
-    }
-
-    printf("%d\n", count);
 }
